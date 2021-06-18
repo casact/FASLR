99de22ae6acedfa53066dc00caec17103e19bff3
genedan <genedan@gmail.com>
2021-06-17 21:20:10 -0500

    Add file menu options for importing, settings.

--- faslr/main.py
+++ faslr/main.py
@@ -22,15 +22,22 @@ class MainWindow(QMainWindow):
         file_menu = QMenu("&File", self)
         menu_bar.addMenu(file_menu)
         menu_bar.addMenu("&Edit")
+        menu_bar.addMenu("&Tools")
         help_menu = menu_bar.addMenu("&Help")
 
         self.new_action = QAction(self)
+        self.connection_action = QAction("&Connection", self)
         self.new_action.setText("&New Project")
+        self.import_action = QAction("&Import Project")
+
+        self.settings_action = QAction("&Settings")
 
         self.about_action = QAction("&About", self)
 
-
+        file_menu.addAction(self.connection_action)
         file_menu.addAction(self.new_action)
+        file_menu.addAction(self.import_action)
+        file_menu.addAction(self.settings_action)
 
         help_menu.addAction(self.about_action)
 
