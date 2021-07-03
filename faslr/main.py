762ca095ade644bb8f155855fafaa2f88ceefb13
genedan <genedan@gmail.com>
2021-07-02 23:12:36 -0500

    Add status bar tips.

--- faslr/main.py
+++ faslr/main.py
@@ -7,6 +7,7 @@ from PyQt5.QtWidgets import (
     QApplication,
     QMainWindow,
     QMenu,
+    QStatusBar
 )
 
 
@@ -14,24 +15,38 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        # Flag to determine whether there is an active database connection. Most project-related functions
+        # should be disabled unless a connection is established.
+        connection_established = False
+
         self.setMinimumWidth(400)
 
         self.setWindowTitle("FASLR - Free Actuarial System for Loss Reserving")
 
         menu_bar = self.menuBar()
 
-        self.new_action = QAction(self)
         self.connection_action = QAction("&Connection", self)
         self.connection_action.setShortcut(QKeySequence("Ctrl+Shift+c"))
         self.connection_action.setStatusTip("Edit database connection.")
 
-        self.new_action.setText("&New Project")
+        self.new_action = QAction("&New Project", self)
+        self.new_action.setShortcut(QKeySequence("Ctrl+n"))
+        self.new_action.setStatusTip("Create new project.")
+
         self.import_action = QAction("&Import Project")
+        self.import_action.setShortcut(QKeySequence("Ctrl+Shift+i"))
+        self.import_action.setStatusTip("Import a project from another data source.")
+
         self.engine_action = QAction("&Select Engine")
+        self.engine_action.setShortcut("Ctrl+shift+e")
+        self.engine_action.setStatusTip("Select a reserving engine.")
 
         self.settings_action = QAction("&Settings")
+        self.settings_action.setShortcut("Ctrl+Shift+t")
+        self.settings_action.setStatusTip("Open settings dialog box.")
 
         self.about_action = QAction("&About", self)
+        self.about_action.setStatusTip("About")
 
         file_menu = QMenu("&File", self)
         menu_bar.addMenu(file_menu)
@@ -47,6 +62,8 @@ class MainWindow(QMainWindow):
         tools_menu.addAction(self.engine_action)
 
         help_menu.addAction(self.about_action)
+
+        self.setStatusBar(QStatusBar(self))
 
 
 app = QApplication(sys.argv)
