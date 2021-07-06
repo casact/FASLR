12b7548a121d89497e0d9a0fa3ae109d487334d8
genedan <genedan@gmail.com>
2021-07-05 22:08:06 -0500

    Initialize work on database connection and test connectivity.

---
+++ faslr/connection.py
@@ -0,0 +1,16 @@
+import os
+import sqlalchemy as sa
+import schema
+
+from sqlalchemy.orm import sessionmaker
+
+if not os.path.exists('db'):
+    os.makedirs('db')
+    engine = sa.create_engine(
+        'sqlite:///db/test.db',
+        echo=True
+    )
+    session = sessionmaker(bind=engine)
+    schema.Base.metadata.create_all(engine)
+    session = session()
+    connection = engine.connect()
