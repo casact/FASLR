fc723b2db05751250924404a5b99a8499e275a09
genedan <genedan@gmail.com>
2021-07-07 20:59:58 -0500

    Move body out of if statement, was causing db creation to be skipped if directory already exists

--- faslr/connection.py
+++ faslr/connection.py
@@ -6,11 +6,11 @@ from sqlalchemy.orm import sessionmaker
 
 if not os.path.exists('db'):
     os.makedirs('db')
-    engine = sa.create_engine(
-        'sqlite:///db/test.db',
-        echo=True
-    )
-    session = sessionmaker(bind=engine)
-    schema.Base.metadata.create_all(engine)
-    session = session()
-    connection = engine.connect()
+engine = sa.create_engine(
+    'sqlite:///db/test.db',
+    echo=True
+)
+session = sessionmaker(bind=engine)
+schema.Base.metadata.create_all(engine)
+session = session()
+connection = engine.connect()
