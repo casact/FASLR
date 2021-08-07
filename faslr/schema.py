4e564467014414bdbf7d70c8c3771ef602eed480
genedan <genedan@gmail.com>
2021-08-06 21:51:08 -0500

    Update project table to include line of business, country, and state.

--- faslr/schema.py
+++ faslr/schema.py
@@ -1,7 +1,8 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import (
     Column,
-    Integer
+    Integer,
+    String
 )
 
 Base = declarative_base()
@@ -15,7 +16,17 @@ class ProjectTable(Base):
         primary_key=True
     )
 
+    country = Column(String)
+    state = Column(String)
+    line_of_business = Column(String)
+
     def __repr__(self):
         return "<ProjectTable(" \
+               "country='%s', " \
+               "state='%s', " \
+               "line_of_business='%s', " \
                ")>" % (
+                self.country,
+                self.state,
+                self.line_of_business
                 )
