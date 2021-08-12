5d933f61fac8aa0a887de81a7c3e2d56da2ee397
genedan <genedan@gmail.com>
2021-08-11 21:38:14 -0500

    Move project dialog to its own class.

---
+++ faslr/project.py
@@ -0,0 +1,24 @@
+from PyQt5.QtGui import (
+    QColor,
+    QFont
+)
+
+from PyQt5.QtGui import QStandardItem
+
+
+class ProjectItem(QStandardItem):
+    def __init__(
+            self,
+            text='',
+            font_size=12,
+            set_bold=False,
+            text_color=QColor(0, 0, 0)
+    ):
+        super().__init__()
+
+        project_font = QFont('Open Sans', font_size)
+        project_font.setBold(set_bold)
+
+        self.setForeground(text_color)
+        self.setFont(project_font)
+        self.setText(text)
