3a89518e5cbe9def9f5433fa61cc598f2a40870d
genedan <genedan@gmail.com>
2021-08-13 18:31:37 -0500

    Change formatting of triangle values to numeric, fill in uncompleted portion in grey.

--- faslr/triangle_model.py
+++ faslr/triangle_model.py
@@ -1,5 +1,9 @@
+import numpy as np
 from PyQt5 import QtCore
+
 from PyQt5.QtCore import Qt
+
+from PyQt5.QtGui import QColor
 
 
 class TriangleModel(QtCore.QAbstractTableModel):
@@ -8,10 +12,27 @@ class TriangleModel(QtCore.QAbstractTableModel):
         super(TriangleModel, self).__init__()
         self._data = data
 
+        self.n_rows = self.rowCount()
+        self.n_columns = self.columnCount()
+
     def data(self, index, role=None):
         if role == Qt.DisplayRole:
             value = self._data.iloc[index.row(), index.column()]
-            return str(value)
+            if str(value) == "nan":
+                display_value = ""
+            else:
+                display_value = "{0:,.0f}".format(value)
+                display_value = str(display_value)
+            self.setData(self.index(index.row(), index.column()), QtCore.QVariant(QtCore.Qt.AlignRight),
+                         QtCore.Qt.TextAlignmentRole)
+
+            return display_value
+
+        if role == Qt.TextAlignmentRole:
+            return Qt.AlignRight
+
+        if role == Qt.BackgroundRole and (index.column() >= self.n_rows - index.row()):
+            return QColor(238, 237, 238)
 
     def rowCount(self, parent=None, *args, **kwargs):
         return self._data.shape[0]
