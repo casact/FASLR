309e580654b8d6398e85ba3133edf8acfb947e98
genedan <genedan@gmail.com>
2021-09-09 23:39:46 -0500

    Create QStackedWidget to toggle between configuration settings.
    Create layouts for settings specifying connecting to a db upon startup and also being able to reset the default connection.

--- faslr/settings.py
+++ faslr/settings.py
@@ -2,23 +2,40 @@
 Module containing classes pertaining to application settings.
 """
 
-from constants import SETTINGS_LIST
+import configparser
+
+from constants import (
+    QT_FILEPATH_OPTION,
+    SETTINGS_LIST
+)
 
 from PyQt5.QtCore import (
     QAbstractListModel,
+    QObjectCleanupHandler,
     Qt
 )
 
 from PyQt5.QtWidgets import (
     QDialog,
     QDialogButtonBox,
+    QFileDialog,
     QFrame,
     QHBoxLayout,
+    QLabel,
     QListView,
+    QPushButton,
+    QWidget,
     QVBoxLayout,
     QSplitter,
+    QStackedLayout,
+    QStackedWidget
 )
 
+config_path = 'faslr/faslr.ini'
+config = configparser.ConfigParser()
+config.read(config_path)
+config.sections()
+startup_db = config['STARTUP_CONNECTION']['startup_db']
 
 class SettingsListModel(QAbstractListModel):
     """
@@ -59,11 +76,21 @@ class SettingsDialog(QDialog):
         self.list_model = SettingsListModel(SETTINGS_LIST)
 
         self.list_pane.setModel(self.list_model)
-        self.placeholder = QFrame()
+        # self.placeholder = QWidget()
+        self.configuration_layout = QStackedWidget()
+        self.startup_connected_container = QWidget()
+        self.startup_unconnected_container = QWidget()
+        self.startup_unconnected_layout()
+        self.startup_connected_layout(db_filename=startup_db)
+        self.configuration_layout.addWidget(self.startup_connected_container)
+        self.configuration_layout.addWidget(self.startup_unconnected_container)
+        self.configuration_layout.setCurrentIndex(0)
+        self.list_pane.setCurrentIndex(self.list_model.index(0))
+        self.update_config_layout(self.list_pane.currentIndex())
 
         splitter = QSplitter(Qt.Horizontal)
         splitter.addWidget(self.list_pane)
-        splitter.addWidget(self.placeholder)
+        splitter.addWidget(self.configuration_layout)
         splitter.setStretchFactor(1, 1)
         splitter.setSizes([250, 150])
 
@@ -75,3 +102,52 @@ class SettingsDialog(QDialog):
 
     def update_config_layout(self, index):
         print(index.data())
+
+        if index.data() == "Startup":
+            print(startup_db)
+            if startup_db == "None":
+                print('asdf')
+                self.configuration_layout.setCurrentIndex(0)
+            else:
+                print('zxcv')
+                self.configuration_layout.setCurrentIndex(1)
+
+    def startup_unconnected_layout(self):
+        layout = QVBoxLayout()
+        label = QLabel("Upon startup, connect to: ")
+        connect_button = QPushButton("Add Connection")
+        layout.addWidget(label)
+        layout.addWidget(connect_button)
+        layout.setAlignment(Qt.AlignTop)
+        connect_button.clicked.connect(self.set_connection)
+        self.startup_connected_container.setLayout(layout)
+
+    def startup_connected_layout(self, db_filename):
+        layout = QVBoxLayout()
+        label = QLabel("Upon startup, connect to: ")
+        db_label = QLabel(db_filename)
+        reset_connection = QPushButton("Reset Connection")
+        layout.addWidget(label)
+        layout.addWidget(db_label)
+        layout.addWidget(reset_connection)
+        layout.setAlignment(Qt.AlignTop)
+        self.startup_unconnected_container.setLayout(layout)
+
+    def set_connection(self):
+        db_filename = QFileDialog.getOpenFileName(
+            self,
+            'OpenFile',
+            '',
+            "Sqlite Database (*.db)",
+            options=QT_FILEPATH_OPTION)[0]
+
+        print(db_filename)
+        self.startup_connected_layout(db_filename=db_filename)
+        global startup_db
+        startup_db = db_filename
+        config['STARTUP_CONNECTION']['startup_db'] = db_filename
+        with open(config_path, 'w') as configfile:
+            config.write(configfile)
+        self.configuration_layout.setCurrentIndex(1)
+
+
