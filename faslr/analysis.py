0df54a44d5dd3b6d5df3b794ecc68926185f0d5d
genedan <genedan@gmail.com>
2022-02-11 19:16:29 -0600

    Add combo box to toggle between values and link ratios.

--- faslr/analysis.py
+++ faslr/analysis.py
@@ -2,7 +2,10 @@ from utilities.accessors import get_column
 
 from chainladder import Triangle
 
+from PyQt5.QtCore import Qt
+
 from PyQt5.QtWidgets import (
+    QComboBox,
     QTabWidget,
     QVBoxLayout
 )
@@ -11,6 +14,8 @@ from triangle_model import (
     TriangleModel,
     TriangleView
 )
+
+value_types = ['Values', 'Link Ratios']
 
 
 class AnalysisTab(QTabWidget):
@@ -26,10 +31,16 @@ class AnalysisTab(QTabWidget):
 
         self.layout = QVBoxLayout()
 
+        self.value_box = QComboBox()
+        self.value_box.setFixedWidth(200)
+        self.value_box.addItems(value_types)
+
         self.column_tab = ColumnTab()
         self.column_tab.setTabPosition(QTabWidget.West)
 
         self.column_list = list(self.triangle.columns)
+
+        self.triangle_views = {}
 
         for i in self.column_list:
 
@@ -39,15 +50,16 @@ class AnalysisTab(QTabWidget):
                 lob=self.lob
             )
 
-            triangle_view = TriangleView()
+            self.triangle_views[i] = TriangleView()
 
             triangle_frame = triangle_column.to_frame()
 
-            triangle_model = TriangleModel(triangle_frame)
-            triangle_view.setModel(triangle_model)
+            triangle_model = TriangleModel(triangle_frame, 'value')
+            self.triangle_views[i].setModel(triangle_model)
 
-            self.column_tab.addTab(triangle_view, i)
+            self.column_tab.addTab(self.triangle_views[i], i)
 
+        self.layout.addWidget(self.value_box, alignment=Qt.AlignRight)
         self.layout.addWidget(self.column_tab)
 
         self.setLayout(self.layout)
@@ -74,6 +86,26 @@ class AnalysisTab(QTabWidget):
             }
             """
         )
+
+        self.value_box.currentTextChanged.connect(self.update_value_type)
+
+    def update_value_type(self):
+
+        index = self.column_tab.currentIndex()
+        tab_name = self.column_tab.tabText(index)
+
+        if self.value_box.currentText() == "Link Ratios":
+            value_type = 'ratio'
+            link_ratios = self.triangle.link_ratio
+
+            triangle_frame = link_ratios[self.column_list[index]].to_frame()
+
+        else:
+            value_type = 'value'
+            triangle_frame = self.triangle[self.column_list[index]].to_frame()
+
+        triangle_model = TriangleModel(triangle_frame, value_type)
+        self.triangle_views[tab_name].setModel(triangle_model)
 
 
 class ColumnTab(QTabWidget):
