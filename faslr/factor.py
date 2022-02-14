16c7dc4273866c49e4981a6cf9d07a6a9a680298
genedan <genedan@gmail.com>
2022-02-13 19:13:22 -0600

    Begin work on model and view for development factors.

---
+++ faslr/factor.py
@@ -0,0 +1,100 @@
+import csv
+import io
+
+from PyQt5.QtCore import (
+    QAbstractTableModel,
+    Qt,
+    QVariant
+)
+
+from PyQt5.QtGui import (
+    QKeySequence
+)
+
+from PyQt5.QtWidgets import (
+    QAction,
+    qApp,
+    QMenu,
+    QTableView
+)
+
+
+class FactorModel(QAbstractTableModel):
+
+    def __init__(self, data):
+        super(FactorModel, self).__init__()
+        self._data = data
+        self.n_rows = self.rowCount()
+        self.n_columns = self.columnCount()
+
+    def data(self, index, role=None):
+        if role == Qt.DisplayRole:
+            value = self._data.iloc[index.row(), index.column()]
+            if str(value) == "nan":
+                display_value = ""
+            else:
+                display_value = "{0:,.3f}".format(value)
+
+                display_value = str(display_value)
+            self.setData(self.index(index.row(), index.column()), QVariant(Qt.AlignRight),
+                         Qt.TextAlignmentRole)
+
+            return display_value
+
+        if role == Qt.TextAlignmentRole:
+            return Qt.AlignRight
+
+    def rowCount(self, parent=None, *args, **kwargs):
+        return self._data.shape[0]
+
+    def columnCount(self, parent=None, *args, **kwargs):
+        return self._data.shape[1]
+
+    def headerData(self, p_int, qt_orientation, role=None):
+        # section is the index of the column/row.
+        if role == Qt.DisplayRole:
+            if qt_orientation == Qt.Horizontal:
+                return str(self._data.columns[p_int])
+
+            if qt_orientation == Qt.Vertical:
+                return str(self._data.index[p_int])
+
+
+class FactorView(QTableView):
+    def __init__(self):
+        super().__init__()
+
+        self.copy_action = QAction("&Copy", self)
+        self.copy_action.setShortcut(QKeySequence("Ctrl+c"))
+        self.copy_action.setStatusTip("Copy selection to clipboard.")
+        # noinspection PyUnresolvedReferences
+        self.copy_action.triggered.connect(self.copy_selection)
+
+    def contextMenuEvent(self, event):
+        """
+        When right-clicking a cell, activate context menu.
+
+        :param: event
+        :return:
+        """
+        menu = QMenu()
+        menu.addAction(self.copy_action)
+        menu.exec(event.globalPos())
+
+    def copy_selection(self):
+        """Method to copy selected values to clipboard, so they can be pasted elsewhere, like Excel."""
+        selection = self.selectedIndexes()
+        if selection:
+            rows = sorted(index.row() for index in selection)
+            columns = sorted(index.column() for index in selection)
+            rowcount = rows[-1] - rows[0] + 1
+            colcount = columns[-1] - columns[0] + 1
+            table = [[''] * colcount for _ in range(rowcount)]
+            for index in selection:
+                row = index.row() - rows[0]
+                column = index.column() - columns[0]
+                table[row][column] = index.data()
+            stream = io.StringIO()
+            csv.writer(stream, delimiter='\t').writerows(table)
+            qApp.clipboard().setText(stream.getvalue())
+        return
