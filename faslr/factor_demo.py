26f488f35a8515ea1b75fe4c04b70bed8958e311
genedan <genedan@gmail.com>
2022-02-15 22:26:11 -0600

    Update factor demo to use development tab.

--- faslr/factor_demo.py
+++ faslr/factor_demo.py
@@ -1,42 +1,19 @@
-import chainladder as cl
 import sys
 
-from factor import FactorView, FactorModel
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
+
+from methods.development import DevelopmentTab
+from PyQt5.QtWidgets import QApplication
 from utilities.sample import load_sample
 
 test = load_sample("us_industry_auto")
 
-test = test['Reported Claims']
-
-dev = cl.Development().fit(test)
-
-dev_frame = dev.ldf_.to_frame()
-
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        layout = QVBoxLayout()
-
-        factor_model = FactorModel(dev_frame)
-        factor_view = FactorView()
-
-        factor_view.setModel(factor_model)
-        factor_view.verticalHeader().hide()
-        # factor_view.horizontalHeader().hide()
-        layout.addWidget(factor_view)
-
-        self.container = QWidget()
-        self.container.setLayout(layout)
-        self.setCentralWidget(self.container)
-
-
 app = QApplication(sys.argv)
 
-window = MainWindow()
+mytab = DevelopmentTab(
+    triangle=test,
+    column='Paid Claims'
+)
 
-window.show()
+mytab.show()
 
 app.exec_()
