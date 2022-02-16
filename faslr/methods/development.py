ea0d42ee2e3add4361b07d0b514986b3b5d261e4
genedan <genedan@gmail.com>
2022-02-15 22:26:21 -0600

    Add development factors to development tab.

--- faslr/methods/development.py
+++ faslr/methods/development.py
@@ -1,4 +1,7 @@
+import chainladder as cl
 from chainladder import Triangle
+
+from factor import FactorModel, FactorView
 
 from triangle_model import (
     TriangleModel,
@@ -23,12 +26,22 @@ class DevelopmentTab(QWidget):
         self.link_ratios = triangle[column].link_ratio
         self.link_frame = self.link_ratios.to_frame()
 
+        self.development_factors = cl.Development().fit(triangle)
+
+        self.development_frame = self.development_factors.ldf_.to_frame()
+
         self.triangle_model = TriangleModel(self.link_frame, "ratio")
         self.triangle_view = TriangleView()
         self.triangle_view.setModel(self.triangle_model)
 
+        self.factor_model = FactorModel(self.development_frame)
+        self.factor_view = FactorView()
+        self.factor_view.setModel(self.factor_model)
+
         self.layout.addWidget(self.triangle_view)
+        self.layout.addWidget(self.factor_view)
         self.setLayout(self.layout)
+
 
 
 
