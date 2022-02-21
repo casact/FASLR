fae591e1e51271153f4a3d1f1397b5c3876acfaa
genedan <genedan@gmail.com>
2022-02-21 16:04:22 -0600

    Update factor demo.

--- faslr/factor_demo.py
+++ faslr/factor_demo.py
@@ -1,13 +1,10 @@
 import sys
 
-
 from methods.development import DevelopmentTab
 from PyQt5.QtWidgets import QApplication
 from utilities.sample import load_sample
 
 test = load_sample("us_industry_auto")
-
-test["Paid Claims"].valuation_correlation(p_critical=0.1, total=False).z_critical
 
 app = QApplication(sys.argv)
 
