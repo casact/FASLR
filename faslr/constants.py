3d9b4905edf7528f09acc971f087cef9f70080fd
genedan <genedan@gmail.com>
2022-03-11 23:57:58 -0600

    move stock LDF averages to constants

--- faslr/constants.py
+++ faslr/constants.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import os
 from os.path import dirname
 from PyQt5.QtWidgets import QFileDialog
@@ -20,3 +21,20 @@ ROOT_PATH = dirname(dirname(os.path.realpath(__file__)))
 CONFIG_PATH = os.path.join(ROOT_PATH, 'faslr.ini')
 
 TEMPLATES_PATH = os.path.join(dirname(os.path.realpath(__file__)), 'templates')
+
+TEMP_LDF_LIST = pd.DataFrame(
+    data=[
+        [True, "All-year volume-weighted", "Volume", "9"],
+        [False, "3-year volume-weighted", "Volume", "3"],
+        [False, "5-year volume-weighted", "Volume", "5"],
+    ],
+    columns=["Selected", "Label", "Type", "Number of Years"]
+)
+
+LDF_AVERAGES = {
+            # 'Geometric': 'geometric',
+            # 'Medial': 'medial',
+            'Regression': 'regression',
+            'Straight': 'simple',
+            'Volume': 'volume'
+}
