3db1a0b9788cc18351785092dc6630a04d7f63db
genedan <genedan@gmail.com>
2022-03-14 22:19:31 -0500

    Add annotations.

--- faslr/utilities/style_parser.py
+++ faslr/utilities/style_parser.py
@@ -3,25 +3,42 @@ import re
 
 from bs4 import BeautifulSoup
 from chainladder import Triangle
+from matplotlib.colors import Colormap
 from pandas import DataFrame
 
 from faslr.style.triangle import LOWER_DIAG_COLOR
 
 
-def parse_styler(triangle: Triangle, cmap) -> DataFrame:
+def parse_styler(
+        triangle: Triangle,
+        cmap: [str, Colormap]
+) -> DataFrame:
+    """
+    Takes a triangle, calculates the heatmap, and then returns a dataframe of the colors. Used in
+    implementing the heatmap functionality from chainladder to FASLR table.
+    :param triangle:
+    :param cmap:
+    :return:
+    """
 
     heatmap_html = triangle.link_ratio.heatmap(cmap=cmap).data
 
     parsed_html = BeautifulSoup(heatmap_html, 'html.parser')
     css = str(parsed_html.find('style'))
 
+    # First line is the style header
     buf = io.StringIO(str(css))
 
+    # Initialize dataframe containing FASLR table background color
     color_triangle = triangle.link_ratio.to_frame()
     color_triangle = color_triangle.astype(str)
     color_triangle.loc[:] = LOWER_DIAG_COLOR.name()
+
+    # Count the number of background colors to apply
     n_colors = css.count("background")
     buf.readline()
+
+    # For each color, find the cells to which it applies
     for i in range(n_colors):
         rowcols = buf.readline()
         result_rows = [x.start() for x in re.finditer('row', rowcols)]
@@ -36,7 +53,3 @@ def parse_styler(triangle: Triangle, cmap) -> DataFrame:
             color_triangle.iloc[[row], [col]] = color
 
     return color_triangle
-
-
-# from PyQt5.QtGui import QColor
-# QColor(df.iloc[[0], [0]].squeeze())
