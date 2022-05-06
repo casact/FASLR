de3d2f19f23339a892adcb6c8e773dfb5b643f8d
genedan <genedan@gmail.com>
2022-05-06 18:01:12 -0500

    Add Mack 1993 tests. Closes #52. Closes #52.

--- faslr/analysis.py
+++ faslr/analysis.py
@@ -8,6 +8,8 @@ from PyQt5.QtGui import QColor
 
 from PyQt5.QtWidgets import (
     QComboBox,
+    QLabel,
+    QStackedWidget,
     QTabWidget,
     QVBoxLayout,
     QWidget
@@ -18,7 +20,11 @@ from faslr.triangle_model import (
     TriangleView
 )
 
-value_types = ['Values', 'Link Ratios']
+value_types = [
+    'Values',
+    'Link Ratios',
+    'Diagnostics'
+]
 
 
 class AnalysisTab(QWidget):
@@ -44,8 +50,10 @@ class AnalysisTab(QWidget):
         self.column_list = list(self.triangle.columns)
 
         self.triangle_views = {}
+        self.analysis_containers = {}
 
         for i in self.column_list:
+            print(i)
 
             triangle_column = get_column(
                 triangle=self.triangle,
@@ -54,11 +62,50 @@ class AnalysisTab(QWidget):
             )
 
             self.triangle_views[i] = TriangleView()
+            self.analysis_containers[i] = QStackedWidget()
+            self.analysis_containers[i].addWidget(self.triangle_views[i])
+
+            valuation_correlation = triangle_column.valuation_correlation(
+                p_critical=0.1,
+                total=True
+            ).z_critical.values[0][0]
+
+            development_correlation = triangle_column.development_correlation(
+                p_critical=0.5
+            ).t_critical.values[0][0]
+
+            if valuation_correlation:
+                valuation_pass = "Fail"
+            else:
+                valuation_pass = "Pass"
+
+            if development_correlation:
+                development_pass = "Fail"
+            else:
+                development_pass = "Pass"
+
+            layout = QVBoxLayout()
+            layout.addWidget(QLabel("Mack Valuation Correlation Test: " + valuation_pass))
+            layout.addWidget(QLabel("Mack Development Correlation Test: " + development_pass))
+            layout.setAlignment(Qt.AlignTop)
+            dw = DiagnosticWidget()
+
+            dw.setLayout(layout)
+            self.analysis_containers[i].addWidget(dw)
 
             triangle_model = TriangleModel(triangle_column, 'value')
             self.triangle_views[i].setModel(triangle_model)
 
-            self.column_tab.addTab(self.triangle_views[i], i)
+            self.analysis_containers[i].setStyleSheet(
+                """
+                DiagnosticWidget {
+                    border: 1px solid darkgrey;
+                    background: rgb(230, 230, 230);
+                }
+                """
+            )
+
+            self.column_tab.addTab(self.analysis_containers[i], i)
 
         self.layout.addWidget(self.value_box, alignment=Qt.AlignRight)
         self.layout.addWidget(self.column_tab)
@@ -85,6 +132,7 @@ class AnalysisTab(QWidget):
               background: rgb(245, 245, 245); 
               margin-bottom: -1px; 
             }
+            
             """
         )
 
@@ -103,17 +151,31 @@ class AnalysisTab(QWidget):
             value_type = 'ratio'
             triangle = self.triangle.link_ratio
 
-        else:
+        elif self.value_box.currentText() == "Values":
             value_type = 'value'
+            triangle = self.triangle
+
+        else:
+            value_type = "diagnostics"
             triangle = self.triangle
 
         for i in range(len(self.column_list)):
             index = i
             tab_name = self.column_tab.tabText(index)
-            triangle_column = triangle[self.column_list[index]]
 
-            triangle_model = TriangleModel(triangle_column, value_type)
-            self.triangle_views[tab_name].setModel(triangle_model)
+            if value_type != "diagnostics":
+                triangle_column = triangle[self.column_list[index]]
+
+                triangle_model = TriangleModel(triangle_column, value_type)
+                self.triangle_views[tab_name].setModel(triangle_model)
+                self.analysis_containers[tab_name].setCurrentIndex(0)
+            else:
+                self.analysis_containers[tab_name].setCurrentIndex(1)
+
+
+class DiagnosticWidget(QWidget):
+    def __init__(self):
+        super().__init__()
 
 
 class ColumnTab(QTabWidget):
