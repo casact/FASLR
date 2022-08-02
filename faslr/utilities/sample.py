6bc115f4eb9d633b06f569c4855f9290621140e4
Gene Dan <genedan@gmail.com>
2022-08-01 23:15:08 -0500

    Add sample from Mack 1997 paper.

--- faslr/utilities/sample.py
+++ faslr/utilities/sample.py
@@ -7,21 +7,36 @@ from chainladder import Triangle
 def load_sample(sample_name: str) -> Triangle:
 
     path = os.path.dirname(os.path.abspath(__file__))
-    if sample_name == "us_industry_auto":
+    if sample_name == "mack97":
+        df_csv = pd.read_csv(
+            os.path.join(path, "..", "samples", "mack_1997.csv"))
+    elif sample_name == "us_industry_auto":
         df_csv = pd.read_csv(
             os.path.join(path, "..", "samples", "friedland_us_industry_auto.csv"))
+    elif sample_name == "uspp_incr_case":
+        df_csv = pd.read_csv(
+            os.path.join(path, "..", "samples", "friedland_uspp_auto_increasing_case.csv"))
     elif sample_name == "xyz":
         df_csv = pd.read_csv(
             os.path.join(path, "..", "samples", "friedland_xyz_auto_bi.csv"))
     else:
         raise Exception("Invalid sample name.")
 
-    triangle = cl.Triangle(
-        data=df_csv,
-        origin='Accident Year',
-        development='Calendar Year',
-        columns=['Paid Claims', 'Reported Claims'],
-        cumulative=True
-    )
+    if sample_name != "mack97":
+        triangle = cl.Triangle(
+            data=df_csv,
+            origin='Accident Year',
+            development='Calendar Year',
+            columns=['Paid Claims', 'Reported Claims'],
+            cumulative=True
+        )
+    else:
+        triangle = cl.Triangle(
+            data=df_csv,
+            origin='Accident Year',
+            development='Calendar Year',
+            columns=['Case Incurred'],
+            cumulative=True
+        )
 
     return triangle
