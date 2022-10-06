d66aaf4597eff88dde3551399f7da0a0fee72793
Gene Dan <genedan@gmail.com>
2022-10-05 21:08:19 -0500

    Designate new sub-package for FASLR constants.

---
+++ faslr/constants/__init__.py
@@ -0,0 +1,27 @@
+from faslr.constants.analysis import (
+    VALUE_TYPES,
+    VALUE_TYPES_COMBO_BOX_WIDTH
+)
+
+
+from faslr.constants.development import (
+    LDF_AVERAGES,
+    TEMP_LDF_LIST
+)
+
+from faslr.constants.diagnostics import (
+    MACK_DEVELOPMENT_CRITICAL,
+    MACK_VALUATION_CRITICAL
+)
+
+from faslr.constants.settings import (
+    SETTINGS_LIST
+)
+
+from faslr.constants.general import (
+    BUILD_VERSION,
+    CONFIG_PATH,
+    QT_FILEPATH_OPTION,
+    ROOT_PATH,
+    TEMPLATES_PATH
+)
