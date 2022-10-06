42f66689d5db0511038e83c03dd6307ca275403d
Gene Dan <genedan@gmail.com>
2022-10-06 06:09:53 -0500

    Remove unused imports.

--- faslr/analysis.py
+++ faslr/analysis.py
@@ -18,12 +18,7 @@ from PyQt5.QtCore import Qt
 
 from PyQt5.QtGui import QColor
 
-from PyQt5 import QtWidgets
-from PyQt5.QtGui import QGuiApplication
-from PyQt5.QtCore import QCoreApplication
-
 from PyQt5.QtWidgets import (
-    QApplication,
     QComboBox,
     QFormLayout,
     QGroupBox,
