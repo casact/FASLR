c7269f2a74888bd321c4204d84986cc1e4c81e34
Gene Dan <genedan@gmail.com>
2022-10-15 21:46:00 -0500

    Begin work on import wizard.

--- faslr/data.py
+++ faslr/data.py
@@ -1,14 +1,27 @@
+import numpy as np
+import pandas as pd
+
+from faslr.base_table import (
+    FAbstractTableModel,
+    FTableView
+)
+
 from faslr.constants import (
     QT_FILEPATH_OPTION
 )
 
 from PyQt6.QtCore import (
+    QAbstractTableModel,
     Qt
 )
 
 from PyQt6.QtWidgets import (
     QFileDialog,
+    QFormLayout,
+    QHBoxLayout,
+    QLineEdit,
     QPushButton,
+    QTableView,
     QVBoxLayout,
     QWidget
 )
@@ -17,6 +30,8 @@ from PyQt6.QtWidgets import (
 class DataPane(QWidget):
     def __init__(self, parent=None):
         super().__init__()
+
+        self.wizard = None
 
         self.layout = QVBoxLayout()
         self.upload_btn = QPushButton("Upload")
@@ -27,16 +42,80 @@ class DataPane(QWidget):
         )
         filler = QWidget()
         self.layout.addWidget(filler)
-        self.upload_btn.clicked.connect(self.load_file) # noqa
+        self.upload_btn.pressed.connect(self.start_wizard) # noqa
+
+    def start_wizard(self):
+
+        self.wizard = DataImportWizard()
+
+        self.wizard.show()
+
+
+class DataImportWizard(QWidget):
+    def __init__(
+            self,
+            parent=None
+    ):
+        super().__init__()
+
+        self.setWindowTitle("Import Wizard")
+
+        self.layout = QVBoxLayout()
+        self.upload_form = QFormLayout()
+        self.file_path = QLineEdit()
+        self.upload_btn = QPushButton("Upload File")
+        self.upload_container = QWidget()
+        self.upload_container.setLayout(self.upload_form)
+
+        self.file_path_layout = QHBoxLayout()
+        self.file_path_container = QWidget()
+        self.file_path_container.setLayout(self.file_path_layout)
+        self.file_path_layout.addWidget(self.upload_btn)
+        self.file_path_layout.addWidget(self.file_path)
+
+        self.upload_form.addRow(
+            self.file_path_container
+        )
+
+        self.layout.addWidget(self.upload_container)
+        self.setLayout(self.layout)
+
+        self.upload_btn.pressed.connect(self.load_file) # noqa
+
+        self.upload_sample_model = UploadSampleModel()
+        self.upload_sample_view = UploadSampleView()
+        self.upload_sample_view.setModel(self.upload_sample_model)
+
+        self.layout.addWidget(self.upload_sample_view)
 
     def load_file(self):
 
         filename = QFileDialog.getOpenFileName(
             parent=self,
             caption='Open File',
-            filter='',
-            initialFilter='CSV (*.csv)',
+            filter='CSV (*.csv)',
             options=QT_FILEPATH_OPTION
         )[0]
 
-        print(filename)
+        self.file_path.setText(filename)
+
+
+class UploadSampleModel(FAbstractTableModel):
+    def __init__(self):
+        super().__init__()
+
+        self._data = pd.DataFrame(
+            data={'A': [np.nan, np.nan, np.nan],
+                  'B': [np.nan, np.nan, np.nan],
+                  'C': [np.nan, np.nan, np.nan],
+                  '': [np.nan, np.nan, np.nan]
+            })
+
+
+
+class UploadSampleView(FTableView):
+    def __init__(self):
+        super().__init__()
+
+        # self.horizontalHeader().setStretchLastSection(True)
+        # self.verticalHeader().setStretchLastSection(True)
