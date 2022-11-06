3164709f9bdef7184ecbff6dcd57ad3fdfb0e923
Gene Dan <genedan@gmail.com>
2022-11-05 22:19:00 -0500

    Update test_schema.py.

--- faslr/tests/test_schema.py
+++ faslr/tests/test_schema.py
@@ -1,6 +1,8 @@
 import sqlalchemy as sa
 
 from faslr import schema
+from faslr.schema import UserTable
+from sqlalchemy.orm import sessionmaker
 
 engine = sa.create_engine(
     'sqlite:///test_schema.db',
@@ -8,5 +10,12 @@ engine = sa.create_engine(
 )
 
 schema.Base.metadata.create_all(engine)
+session = sessionmaker(bind=engine)()
 connection = engine.connect()
+
+new_user = UserTable(user_name="Gene")
+
+session.add(new_user)
+session.commit()
+
 connection.close()
