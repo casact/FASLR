d0fdb9e5998295034140f0bd35f22d7021f5b8ce
Gene Dan <genedan@gmail.com>
2022-11-13 19:15:18 -0600

    Move pragma to __init__.py.

--- faslr/utilities/__init__.py
+++ faslr/utilities/__init__.py
@@ -1,1 +1,11 @@
 from faslr.utilities.gui import open_item_tab
+
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
