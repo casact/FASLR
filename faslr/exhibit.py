ef1eed49a10d7d295648487eda32cf47a3c20264
Gene Dan <genedan@gmail.com>
2022-11-14 22:34:24 -0600

    Add exhibit classes.

---
+++ faslr/exhibit.py
@@ -0,0 +1,14 @@
+from faslr.base_table import (
+    FAbstractTableModel,
+    FTableView
+)
+
+
+class ExhibitModel(FAbstractTableModel):
+    def __init__(self):
+        super().__init__()
+
+
+class ExhibitView(FTableView):
+    def __init__(self):
+        super().__init__()
