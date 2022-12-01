f7b1353eb2c41cf779180981a9f0cca8044e0e3d
Gene Dan <genedan@gmail.com>
2022-11-30 21:57:41 -0600

    Update tail.py

--- faslr/tail.py
+++ faslr/tail.py
@@ -1,9 +1,14 @@
+import matplotlib
+
 from chainladder import Triangle
 
 from faslr.base_table import (
     FAbstractTableModel,
     FTableView
 )
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from matplotlib.figure import Figure
 
 from PyQt6.QtWidgets import (
     QLabel,
