b9a76befc53fac07410c5159ff3e7077b4bbdfe7
Gene Dan <genedan@gmail.com>
2022-12-01 22:12:37 -0600

    Update tail.py

--- faslr/tail.py
+++ faslr/tail.py
@@ -17,6 +17,29 @@ from PyQt6.QtWidgets import (
 )
 
 
+class MplCanvas(FigureCanvasQTAgg):
+
+    def __init__(
+            self,
+            parent=None,
+            width=5,
+            height=4,
+            dpi=100
+    ):
+
+        fig = Figure(
+            figsize=(
+                width,
+                height
+            ),
+            dpi=dpi
+        )
+
+        self.axes = fig.add_subplot(111)
+
+        super(MplCanvas, self).__init__(fig)
+
+
 class TailPane(QWidget):
     def __init__(
             self,
