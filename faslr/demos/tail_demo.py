bf89df373bcfebc0f851777b40e09768f4286d0a
Gene Dan <genedan@gmail.com>
2022-12-07 22:18:26 -0600

    Update tail.py

--- faslr/demos/tail_demo.py
+++ faslr/demos/tail_demo.py
@@ -5,6 +5,11 @@ from faslr.tail import (
 )
 
 triangle = cl.load_sample('genins')
-tail = cl.TailConstant(1.10).fit_transform(triangle)
+# tail = cl.TailConstant(1.10).fit_transform(triangle)
+unsmoothed = cl.TailCurve().fit(triangle).ldf
+smoothed = cl.TailCurve(attachment_age=24).fit(triangle.ldf)
+
+
+
 
 tail_pane = TailPane()
