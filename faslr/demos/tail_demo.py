3b76ff80f6e1941510d161cda83f122e07113a31
Gene Dan <genedan@gmail.com>
2022-12-09 21:56:28 -0600

    Update tail demo.

--- faslr/demos/tail_demo.py
+++ faslr/demos/tail_demo.py
@@ -1,15 +1,21 @@
 import chainladder as cl
+import sys
+
+from PyQt6.QtWidgets import QApplication
 
 from faslr.tail import (
     TailPane
 )
 
-triangle = cl.load_sample('genins')
-# tail = cl.TailConstant(1.10).fit_transform(triangle)
-unsmoothed = cl.TailCurve().fit(triangle).ldf
-smoothed = cl.TailCurve(attachment_age=24).fit(triangle.ldf)
+# triangle = cl.load_sample('genins')
+# # tail = cl.TailConstant(1.10).fit_transform(triangle)
+# unsmoothed = cl.TailCurve().fit(triangle).ldf
+# smoothed = cl.TailCurve(attachment_age=24).fit(triangle.ldf)
 
-
-
+app = QApplication(sys.argv)
 
 tail_pane = TailPane()
+
+tail_pane.show()
+
+app.exec()
