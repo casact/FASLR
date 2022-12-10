c1ed5409a07a8e1fd90fb82309df1138973eb1ef
Gene Dan <genedan@gmail.com>
2022-12-09 21:56:45 -0600

    Add configuration section, sample graph to tail pane.

--- faslr/tail.py
+++ faslr/tail.py
@@ -1,3 +1,4 @@
+import chainladder as cl
 import matplotlib
 
 from chainladder import Triangle
@@ -7,14 +8,29 @@ from faslr.base_table import (
     FTableView
 )
 
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from faslr.base_classes import FDoubleSpinBox
+
+from matplotlib.backends.backend_qt5agg import (
+    FigureCanvasQTAgg,
+    NavigationToolbar2QT as NavigationToolbar
+)
+
 from matplotlib.figure import Figure
 
 from PyQt6.QtWidgets import (
-    QLabel,
-    QPushButton,
-    QWidget
+    QGroupBox,
+    QHBoxLayout,
+    QRadioButton,
+    QWidget,
+    QVBoxLayout
 )
+
+matplotlib.use('Qt5Agg')
+
+
+triangle = cl.load_sample('genins')
+unsmoothed = cl.TailCurve().fit(triangle).ldf_
+smoothed = cl.TailCurve(attachment_age=24).fit(triangle).ldf_
 
 
 class MplCanvas(FigureCanvasQTAgg):
@@ -43,9 +59,83 @@ class MplCanvas(FigureCanvasQTAgg):
 class TailPane(QWidget):
     def __init__(
             self,
-            triangle: Triangle
+            triangle: Triangle = None
     ):
         super().__init__()
+
+        sc = MplCanvas(
+            self,
+            # width=5,
+            # height=4,
+            dpi=100
+        )
+        sc.axes.plot(unsmoothed.development, unsmoothed.T.iloc[:, 0], label='Unsmoothed')
+        sc.axes.plot(unsmoothed.development, smoothed.T.iloc[:, 0], label='Age 24+ Smoothed')
+        sc.axes.set_title("Selected Link Ratio")
+
+        # main layout
+        layout = QHBoxLayout()
+
+        tail_config = QWidget()
+        ly_tail_config = QVBoxLayout()
+        tail_config.setLayout(ly_tail_config)
+
+        gb_tail_type = QGroupBox("Tail Type")
+        ly_tail_type = QHBoxLayout()
+        constant_btn = QRadioButton('Constant')
+        curve_btn = QRadioButton('Curve')
+        bondy_btn = QRadioButton('Bondy')
+        clark_btn = QRadioButton('Clark')
+
+        ly_tail_type.addWidget(
+            constant_btn
+        )
+
+        ly_tail_type.addWidget(
+            curve_btn
+        )
+
+        ly_tail_type.addWidget(
+            bondy_btn
+        )
+
+        ly_tail_type.addWidget(
+            clark_btn
+        )
+
+        gb_tail_type.setLayout(ly_tail_type)
+
+        config_width = gb_tail_type.sizeHint().width()
+
+        gb_tail_type.setFixedWidth(config_width)
+        gb_tail_params = QGroupBox("Tail Parameters")
+        ly_tail_params = QVBoxLayout()
+        gb_tail_params.setLayout(ly_tail_params)
+
+        gb_tail_params.setFixedWidth(config_width)
+
+        sb_tail_constant = FDoubleSpinBox(
+            label='Tail Constant: ',
+            value=1.00,
+            single_step=.01
+        )
+
+        sb_decay = FDoubleSpinBox(
+            label='Decay: ',
+            value=0.5,
+            single_step=.01
+        )
+
+        ly_tail_params.addWidget(sb_tail_constant)
+        ly_tail_params.addWidget(sb_decay)
+
+        ly_tail_config.addWidget(gb_tail_type)
+        ly_tail_config.addWidget(gb_tail_params)
+
+        layout.addWidget(tail_config, stretch=0)
+        layout.addWidget(sc, stretch=1)
+
+        self.setLayout(layout)
 
 
 class TailTableModel(FAbstractTableModel):
