278f6702cc72db8693d616702d6c5ff0515bb156
Gene Dan <genedan@gmail.com>
2022-12-10 17:19:59 -0600

    Add FSpinBox - a combination of label and (regular) spinbox.

--- faslr/base_classes.py
+++ faslr/base_classes.py
@@ -2,6 +2,7 @@ from PyQt6.QtWidgets import (
     QHBoxLayout,
     QLabel,
     QDoubleSpinBox,
+    QSpinBox,
     QWidget
 )
 
@@ -19,6 +20,27 @@ class FDoubleSpinBox(QWidget):
 
         self.label = QLabel(label)
         self.spin_box = QDoubleSpinBox()
+        self.spin_box.setValue(value)
+        self.spin_box.setSingleStep(single_step)
+
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.spin_box)
+        self.setLayout(self.layout)
+
+
+class FSpinBox(QWidget):
+    def __init__(
+            self,
+            label: str,
+            value: int,
+            single_step: int
+    ):
+        super().__init__()
+
+        self.layout = QHBoxLayout()
+
+        self.label = QLabel(label)
+        self.spin_box = QSpinBox()
         self.spin_box.setValue(value)
         self.spin_box.setSingleStep(single_step)
 
