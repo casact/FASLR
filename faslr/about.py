87586a2b41e0cf36d7edcb35462cca6b38d4eb0c
Gene Dan <genedan@gmail.com>
2022-12-15 20:37:40 -0600

    Add links to about dialog.

--- faslr/about.py
+++ faslr/about.py
@@ -1,13 +1,11 @@
 from faslr.constants import (
-    BRANCH_SHA,
     BUILD_VERSION,
     CURRENT_BRANCH,
     CURRENT_COMMIT,
+    CURRENT_COMMIT_LONG,
     ICONS_PATH,
     OCTICONS_PATH
 )
-
-from PyQt6.QtGui import QIcon
 
 from PyQt6.QtCore import (
     Qt
@@ -30,29 +28,40 @@ from PyQt6.QtWidgets import (
 
 class AboutDialog(QDialog):
     # About dialog box
-    def __init__(self, parent=None):
+    def __init__(
+            self,
+            parent=None
+    ):
         super().__init__(parent)
 
         self.setWindowTitle("About")
 
         faslr_version = "FASLR v" + BUILD_VERSION
 
-        faslr_version_link = '<a href="https://github.com/casact/faslr" style="text-decoration:none; color:#000000">' + faslr_version + '</a>'
-        version_label = QLabel(faslr_version_link)
-        version_label.setTextFormat(Qt.TextFormat.RichText)
-        version_label.setTextInteractionFlags(Qt.TextInteractionFlag.TextBrowserInteraction)
-        version_label.setOpenExternalLinks(True)
-        branch_label = QLabel(CURRENT_BRANCH + ' (' + BRANCH_SHA + ')')
-        commit_label = QLabel(CURRENT_COMMIT)
+        faslr_version_link = '<a href="https://github.com/casact/faslr" ' \
+                             'style="text-decoration:none; color:#000000">' + \
+                             faslr_version + '</a>'
 
-        gh_svg = QSvgWidget(ICONS_PATH + "github.svg")
-        gh_svg.setFixedSize(24, 24)
+        branch_link = '<a href="https://github.com/casact/faslr/tree/' + \
+                      CURRENT_BRANCH + '" ' \
+                      'style="text-decoration:none; color:#000000">' + \
+                      CURRENT_BRANCH + '</a>'
 
-        branch_svg = QSvgWidget(OCTICONS_PATH + "git-branch-24.svg")
-        branch_svg.setFixedSize(24, 24)
+        commit_link = '<a href="https://github.com/casact/faslr/commit/' + \
+                      CURRENT_COMMIT_LONG + '" ' \
+                      'style="text-decoration:none; color:#000000">' + \
+                      CURRENT_COMMIT + '</a>'
 
-        commit_svg = QSvgWidget(OCTICONS_PATH + "commit-24.svg")
-        commit_svg.setFixedSize(24, 24)
+        version_label = LinkLabel(faslr_version_link)
+
+        branch_label = LinkLabel(branch_link)
+        commit_label = LinkLabel(commit_link)
+
+        gh_svg = InfoIcon(ICONS_PATH + "github.svg")
+
+        branch_svg = InfoIcon(OCTICONS_PATH + "git-branch-24.svg")
+
+        commit_svg = InfoIcon(OCTICONS_PATH + "commit-24.svg")
 
         layout = QVBoxLayout()
 
@@ -67,10 +76,9 @@ class AboutDialog(QDialog):
         info_layout.addWidget(commit_label, 2, 1)
 
         layout.addWidget(info_grid)
+        info_grid.setFixedSize(info_grid.sizeHint())
 
         self.setLayout(layout)
-
-        # self.setText(faslr_version + "<br /> <br />" + "<a href='https://github.com/genedan/FASLR' style=\"text-decoration:none;\">GitHub Repo</a>")
 
         self.ok_btn = QDialogButtonBox.StandardButton.Ok
         self.button_layout = self.ok_btn
@@ -87,3 +95,29 @@ class AboutDialog(QDialog):
     def ok(self) -> None:
 
         self.close()
+
+
+class InfoIcon(QSvgWidget):
+    def __init__(
+            self,
+            svg_path: str
+    ):
+        super().__init__(svg_path)
+
+        self.setFixedSize(24, 24)
+
+
+class LinkLabel(QLabel):
+    def __init__(
+            self,
+            url: str
+    ):
+        super().__init__(url)
+
+        self.setTextFormat(Qt.TextFormat.RichText)
+
+        self.setTextInteractionFlags(
+            Qt.TextInteractionFlag.TextBrowserInteraction
+        )
+
+        self.setOpenExternalLinks(True)
