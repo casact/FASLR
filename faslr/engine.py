63d034bf0d38d74d2879f691981131b8240449ed
Gene Dan <genedan@gmail.com>
2023-01-30 09:13:05 -0600

    Initialize engine dialog.

---
+++ faslr/engine.py
@@ -0,0 +1,11 @@
+from PyQt6.QtWidgets import (
+    QWidget
+)
+
+
+class EngineDialog(QWidget):
+    """
+    Dialog box to allow user to toggle reserving engine.
+    """
+    def __init__(self):
+        super().__init__()
