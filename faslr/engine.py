8cab828336dcfbe22374905481d23ac2ad66b5a6
Gene Dan <genedan@gmail.com>
2023-02-01 22:17:13 -0600

    Add engine choices.

--- faslr/engine.py
+++ faslr/engine.py
@@ -1,11 +1,40 @@
 from PyQt6.QtWidgets import (
-    QWidget
+    QDialog,
+    QButtonGroup,
+    QRadioButton,
+    QWidget,
+    QVBoxLayout
 )
 
 
-class EngineDialog(QWidget):
+class EngineDialog(QDialog):
     """
     Dialog box to allow user to toggle reserving engine.
     """
-    def __init__(self):
-        super().__init__()
+    def __init__(
+            self,
+            parent=None
+    ):
+        super().__init__(parent)
+
+        self.parent = parent
+
+        self.setWindowTitle('Select Reserving Engine')
+
+        self.layout = QVBoxLayout()
+
+        chainladder_python_btn = QRadioButton('chainladder-python')
+
+        chainladder_r_btn = QRadioButton('chainladder-r')
+
+        trellis_btn = QRadioButton('trellis')
+
+        for btn in [
+            chainladder_python_btn,
+            chainladder_r_btn,
+            trellis_btn
+        ]:
+            self.layout.addWidget(btn)
+
+        self.setLayout(self.layout)
+
