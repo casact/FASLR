95db096f368229b3787ff3e33e2d565cded42836
Gene Dan <genedan@gmail.com>
2023-02-06 21:55:32 -0600

    Add some sample triangles for exhibit builder demo.

--- faslr/demos/exhibit_builder_demo.py
+++ faslr/demos/exhibit_builder_demo.py
@@ -1,11 +1,24 @@
+import chainladder as cl
 import sys
 
 from faslr.exhibit import ExhibitBuilder
+from faslr.utilities.sample import load_sample
+from faslr.utilities.sample import load_sample
 from PyQt6.QtWidgets import QApplication
 
+triangle = load_sample('xyz')
+
+paid = triangle['Paid Claims']
+reported = triangle['Reported Claims']
+
+test = cl.Development().fit_transform(paid)
+
+test2 = cl.Chainladder().fit(test)
 app = QApplication(sys.argv)
 
-exhibit_builder = ExhibitBuilder()
+exhibit_builder = ExhibitBuilder(
+    triangles=[paid, reported]
+)
 
 exhibit_builder.show()
 
