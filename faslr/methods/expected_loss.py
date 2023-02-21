f8ccd27ed34199c57605626e33fc933024dc33fa
Gene Dan <genedan@gmail.com>
2023-02-20 18:50:58 -0600

    Initialize expected loss method module.

---
+++ faslr/methods/expected_loss.py
@@ -0,0 +1,13 @@
+from faslr.base_table import (
+    FAbstractTableModel,
+    FTableView
+)
+
+class ExpectedLossModel(FAbstractTableModel):
+    def __int__(self):
+        super().__init__()
+
+
+class ExpectedLossView(FTableView):
+    def __int__(self):
+        super().__init__()
