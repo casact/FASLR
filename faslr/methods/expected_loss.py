abd6bb114b5c329f930e8302c0e5b1eafecc10fc
Gene Dan <genedan@gmail.com>
2023-02-22 07:47:16 -0600

    Add indexation/model tabs.

--- faslr/methods/expected_loss.py
+++ faslr/methods/expected_loss.py
@@ -3,6 +3,13 @@ from faslr.base_table import (
     FTableView
 )
 
+from PyQt6.QtWidgets import (
+    QDialog,
+    QTabWidget,
+    QWidget,
+    QVBoxLayout
+)
+
 class ExpectedLossModel(FAbstractTableModel):
     def __int__(self):
         super().__init__()
@@ -11,3 +18,28 @@ class ExpectedLossModel(FAbstractTableModel):
 class ExpectedLossView(FTableView):
     def __int__(self):
         super().__init__()
+
+
+class ExpectedLossWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        print('asdf')
+
+        self.setWindowTitle("Expected Loss Method")
+
+        self.layout = QVBoxLayout()
+
+        self.main_tabs = QTabWidget()
+
+        self.indexation = QWidget()
+
+        self.selection_tab = QWidget()
+
+        self.main_tabs.addTab(self.indexation, "Indexation")
+        self.main_tabs.addTab(self.selection_tab, "Model")
+
+        self.layout.addWidget(self.main_tabs)
+
+        self.setLayout(self.layout)
+
