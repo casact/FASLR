032691f403e784ca3c343dad35674ec0c6b5f0b7
Gene Dan <genedan@gmail.com>
2023-02-21 21:33:37 -0600

    Move version string into __init__.py.

--- faslr/__init__.py
+++ faslr/__init__.py
@@ -1,10 +1,12 @@
-from faslr.factor import (
-    LDFAverageModel,
-    LDFAverageView,
-    FactorModel,
-    FactorView
-)
+# from faslr.factor import (
+#     LDFAverageModel,
+#     LDFAverageView,
+#     FactorModel,
+#     FactorView
+# )
+#
+# from faslr.analysis import (
+#     AnalysisTab
+# )
 
-from faslr.analysis import (
-    AnalysisTab
-)
+__version__ = '0.0.6'
