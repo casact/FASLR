4240513b17ccd71a8ceb441f344d1297e6ffc0ee
Gene Dan <genedan@gmail.com>
2023-02-21 21:41:46 -0600

    Extract version number without importing project package.

--- setup.py
+++ setup.py
@@ -1,13 +1,27 @@
+import re
 import setuptools
 
-from faslr.constants import BUILD_VERSION
+# from faslr.constants import BUILD_VERSION
+
+def get_property(
+        prop: str,
+        project: str
+) -> str:
+
+    result = re.search(
+        r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop),
+        open(project + '/__init__.py').read())
+
+    return result.group(1)
+
+project_name = 'faslr'
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="faslr",
-    version=BUILD_VERSION,
+    name=project_name,
+    version=get_property('__version__', project_name),
     author="Gene Dan",
     author_email="genedan@gmail.com",
     description="Free Actuarial System for Loss Reserving",
@@ -17,12 +31,25 @@ setuptools.setup(
     project_urls={
         "Documentation": "https://genedan.com/faslr/docs"
     },
-    install_requires=['chainladder', 'PyQt5'],
+    install_requires=[
+        'bs4',
+        'chainladder',
+        'GitPython',
+        'matplotlib',
+        'numpy',
+        'pandas',
+        'pydata-sphinx-theme',
+        'pytest',
+        'pytest-cov',
+        'PyQt6',
+        'sphinx_design',
+        'sqlalchemy'
+    ],
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: POSIX :: Linux",
     ],
-    python_requires='>=3.6.0',
+    python_requires='>=3.10.0',
 )
