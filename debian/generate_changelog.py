25a95374f235f658174a28b3ea769b1f2e7515d0
Gene Dan <genedan@gmail.com>
2023-02-24 07:37:06 -0600

    Make timezone offset dynamic in changelog script.

--- debian/generate_changelog.py
+++ debian/generate_changelog.py
@@ -1,12 +1,14 @@
 import sys
 sys.path.append('//faslr')
 from datetime import  datetime
-from time import tzname
+import time
 from faslr.constants import BUILD_VERSION
 
-# d = datetime.now(tz=tzname[0])
-datetime_string = d.strftime("%a, %d, %b %Y %H:%M:%S %z")
-datetime_string
+d = datetime.now()
+datetime_string = d.strftime("%a, %d, %b %Y %H:%M:%S ")
+tz_offset = str(int(- time.timezone * 100 / 3600)).zfill(5)
+datetime_string = datetime_string + tz_offset
+
 codename = 'jammy'
 urgency = 'medium'
 package_name = 'faslr'
