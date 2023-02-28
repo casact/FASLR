07101f478de610dc2ba5d52020c326cc5be21ca2
Gene Dan <genedan@gmail.com>
2023-02-28 08:05:05 -0600

    Add example triangle to expected loss demo.

--- faslr/demos/expected_loss_demo.py
+++ faslr/demos/expected_loss_demo.py
@@ -1,8 +1,11 @@
+import chainladder as cl
 import sys
 
 from faslr.methods import (
     ExpectedLossWidget
 )
+
+from faslr.utilities.sample import load_sample
 
 from PyQt6.QtWidgets import (
     QApplication
@@ -10,6 +13,15 @@ from PyQt6.QtWidgets import (
 
 app = QApplication(sys.argv)
 
+triangle = load_sample('auto_bi')
+reported = triangle['Reported Claims']
+paid = triangle['Paid Claims']
+
+reported_dev = cl.TailConstant(tail=1.005).fit_transform(reported)
+reported_ult = cl.Chainladder.fit(reported_dev)
+
+paid_dev = cl.TailConstant(tail=1.05).fit_transform(paid)
+paid_ult = cl.Chainladder.fit(paid_dev)
 
 widget = ExpectedLossWidget()
 
