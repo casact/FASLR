2470f0c83288cc6ffdf7fa7673f58c95436ed928
Gene Dan <genedan@gmail.com>
2023-03-01 23:15:59 -0600

    Initialize chainladder accessors.

---
+++ faslr/utilities/chainladder.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pandas as pd
+
+if TYPE_CHECKING:
+    from chainladder import Chainladder
+    from pandas import DataFrame
+
+
+def table_from_tri(
+        triangle: Chainladder
+) -> DataFrame:
+
+    origin = fetch_origin(triangle=triangle)
+    diagonal = fetch_latest_diagonal(triangle=triangle)
+    column = triangle.X_.columns[0]
+
+    df = pd.DataFrame({
+        'Accident Year': origin,
+        column: diagonal
+    })
+
+    return df
+
+
+def fetch_origin(
+        triangle: Chainladder
+) -> list:
+
+    origin = triangle.X_.origin.to_frame().index.astype(str).tolist()
+
+    return origin
+
+def fetch_latest_diagonal(
+        triangle: Chainladder
+) -> list:
+
+    diagonal = list(triangle.X_.latest_diagonal.to_frame().iloc[:, 0])
+
+    return diagonal
+
