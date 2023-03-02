373f7092352634c364b50808297b8ef1351df438
Gene Dan <genedan@gmail.com>
2023-03-01 23:16:20 -0600

    Add functions to utilities import.

--- faslr/utilities/__init__.py
+++ faslr/utilities/__init__.py
@@ -3,6 +3,16 @@ from faslr.utilities.gui import open_item_tab
 from sqlalchemy import event
 from sqlalchemy.engine import Engine
 
+from faslr.utilities.chainladder import (
+    fetch_latest_diagonal,
+    fetch_origin,
+    table_from_tri
+)
+
+from faslr.utilities.sample import (
+    load_sample
+)
+
 
 @event.listens_for(Engine, "connect")
 def set_sqlite_pragma(dbapi_connection, connection_record):
