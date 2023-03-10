278b1cc28e978a509df41ada95b8a134527c7c4a
odddkidout <nk.vashisat@gmail.com>
2023-03-10 19:30:28 +0530

    improved heatmap

--- faslr/utilities/style_parser.py
+++ faslr/utilities/style_parser.py
@@ -1,13 +1,8 @@
-import io
-import re
-
-from bs4 import BeautifulSoup
 from chainladder import Triangle
 from matplotlib.colors import Colormap
 from pandas import DataFrame
 
 from faslr.style.triangle import LOWER_DIAG_COLOR
-import cssutils
 
 def parse_styler(
         triangle: Triangle,
@@ -22,20 +17,19 @@ def parse_styler(
     """
 
     heatmap_html = triangle.link_ratio.heatmap(cmap=cmap).data
-    parsed_html = BeautifulSoup(heatmap_html, 'html.parser')
-    # Parse css to get the background colors
-    sheet = cssutils.parseString(str(parsed_html.find('style').text))
-    # Initialize dataframe containing FASLR table background color
+    #initialize color_triangle with the lower diagonal color
     color_triangle = triangle.link_ratio.to_frame(origin_as_datetime=False)
     color_triangle = color_triangle.astype(str)
     color_triangle.loc[:] = LOWER_DIAG_COLOR.name()
-    
-    for rule in sheet:
-        if rule.type == rule.STYLE_RULE:
-            for prop in rule.style:
-                if prop.name == 'background-color':
-                    for index in rule.selectorText.split(","):
-                        parts = index.split('_')
-                        color_triangle.iloc[[int(parts[2][3:])], [int(parts[3][3:])]] = prop.value
-    
+    # Parse css to get the background colors
+    splited = str(heatmap_html.split('<style type="text/css">')[1].split('</style>')[0].split('{')).split('}')
+    for item in splited[0:-1]:
+        splited2 = item.split(',')
+        splited2[-2] = splited2[-2].replace(' \'', '')
+        background  = splited2[-1].split('#')[1].split(';')[0]
+        for index in splited2[0:-1]:
+            parts = index.split('_')
+            color_triangle.iloc[[int(parts[2][3:])], [int(parts[3][3:])]] = '#'+background
+        
+        
     return color_triangle
