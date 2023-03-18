b8f84a01e7452dbe637e29a338541feab25c7ff8
Gene Dan <genedan@gmail.com>
2023-03-17 19:01:30 -0500

    Initialize demo for testing index inventory model/view.

---
+++ faslr/demos/index_inventory_demo.py
@@ -0,0 +1,14 @@
+import sys
+
+from faslr.indexation import (
+    IndexInventoryModel,
+    IndexInventoryView
+)
+
+from PyQt6.QtWidgets import (
+    QApplication
+)
+
+app = QApplication(sys.argv)
+
+app.exec()
