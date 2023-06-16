234296a5b3d3d9c3cbd0df5d3b1be41b98ef76fb
Gene Dan <genedan@gmail.com>
2023-06-15 22:31:09 -0500

    DOCS: Add docstring for open_item_tab function.

--- faslr/utilities/gui.py
+++ faslr/utilities/gui.py
@@ -9,6 +9,14 @@ def open_item_tab(
         tab_widget: QTabWidget,
         item_widget: QWidget
 ) -> None:
+    """
+    Used to set focus on a tab right after it is opened.
+
+    :param title: The name of the tab to be opened.
+    :param tab_widget: The QTabWidget object to which the tab will be added.
+    :param item_widget: The widget object representing the tab to be opened.
+    :return:
+    """
 
     tab_widget.addTab(
         item_widget,
