6b982d766c66d6e723e43161a566991b2983d965
Gene Dan <genedan@gmail.com>
2023-06-15 22:30:29 -0500

    TEST: Add pragma to ignore type checking in coverage detection.

--- faslr/utilities/chainladder.py
+++ faslr/utilities/chainladder.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING
 
 import pandas as pd
 
-if TYPE_CHECKING:
+if TYPE_CHECKING: # pragma: no cover
     from chainladder import Chainladder
     from pandas import DataFrame
 
