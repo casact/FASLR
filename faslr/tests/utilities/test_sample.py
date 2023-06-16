15c183b61c1e17f20d734a38838871b100f2510b
Gene Dan <genedan@gmail.com>
2023-06-15 22:32:16 -0500

    TEST: Add tests for load_sample function.

--- faslr/tests/utilities/test_sample.py
+++ faslr/tests/utilities/test_sample.py
@@ -1,3 +1,4 @@
+import pytest
 from faslr.utilities.sample import load_sample
 
 
@@ -7,3 +8,10 @@ def test_us_industry_auto():
 
 def test_xyz():
     load_sample('xyz')
+
+def test_mack97():
+    load_sample('mack97')
+
+def test_invalid_sample():
+    with pytest.raises(Exception):
+        load_sample("blah")
