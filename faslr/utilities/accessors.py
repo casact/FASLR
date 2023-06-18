4df20be4959f562cee4771c77e4ba85881cf7e89
Gene Dan <genedan@gmail.com>
2023-06-18 11:12:32 -0500

    DOCS: Add docstring for get_column accessor. DEPR: Deprecate get_cell_scalar accssor.

--- faslr/utilities/accessors.py
+++ faslr/utilities/accessors.py
@@ -1,19 +1,17 @@
-from typing import Type
-
 from chainladder import Triangle
 
-
-def get_cell_scalar(
-        triangle: Triangle,
-        origin: str,
-        age: int,
-        column: str
-) -> float:
-    cell_scalar = triangle[triangle.origin == origin][
-        (triangle.development >= age) & (triangle.development <= age+12)
-        ][column].link_ratio.to_frame().squeeze()
-
-    return cell_scalar
+# Possibly deprecate this since I have yet a need for it.
+# def get_cell_scalar(
+#         triangle: Triangle,
+#         origin: str,
+#         age: int,
+#         column: str
+# ) -> float:
+#     cell_scalar = triangle[triangle.origin == origin][
+#         (triangle.development >= age) & (triangle.development <= age+12)
+#         ][column].link_ratio.to_frame().squeeze()
+#
+#     return cell_scalar
 
 
 def get_column(
@@ -21,7 +19,14 @@ def get_column(
         column: str,
         lob: [str, None]
 ) -> Triangle:
+    """
+    Extracts a single column from a Triangle class. Assumes the triangle has one company divided by LOBs.
 
+    :param triangle: A ChainLadder Triangle object.
+    :param column: A triangle column (e.g., Paid Loss)
+    :param lob: A line of business.
+    :return: A ChainLadder Triangle, with a single column.
+    """
     if lob is None:
         triangle_column = triangle[column]
     else:
