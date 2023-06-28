d2530d0960db3b6383000a539d57dc00f3a4c916
Gene Dan <genedan@gmail.com>
2023-06-27 22:55:27 -0500

    FEAT: Add containing QTabWidget for data pane demo to allow opening of triangles.

--- faslr/demos/data_pane_demo.py
+++ faslr/demos/data_pane_demo.py
@@ -1,14 +1,26 @@
 import sys
 
+from faslr.constants import DEFAULT_DIALOG_PATH
+from faslr.core import FCore
 from faslr.data import DataPane
+from faslr.__main__ import MainWindow
 
-from PyQt6.QtWidgets import QApplication
-
+from PyQt6.QtWidgets import QApplication, QTabWidget
 app = QApplication(sys.argv)
 
-data_pane = DataPane()
+# main_window = MainWindow()
+# main_window.db = DEFAULT_DIALOG_PATH + '/sample.db'
+
+core = FCore()
+core.set_db(path=DEFAULT_DIALOG_PATH + '/sample.db')
+
+parent_tab = QTabWidget()
+
+data_pane = DataPane(core=core, parent=parent_tab)
 data_pane.setWindowTitle("Project Data Views")
 
-data_pane.show()
+parent_tab.addTab(data_pane, "Data Pane")
+
+parent_tab.show()
 
 app.exec()
