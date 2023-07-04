feaad342052fc3c44ee1909c62291262170bf3db
Gene Dan <genedan@gmail.com>
2023-07-03 21:59:18 -0500

    TEST: Add setup_config fixture to relevant tests in test_project.py.

--- faslr/tests/test_project.py
+++ faslr/tests/test_project.py
@@ -9,22 +9,30 @@ from faslr.project import (
     ProjectTreeView
 )
 
+from pytestqt.qtbot import QtBot
 
-def test_project_dialog(qtbot) -> None:
+
+def test_project_dialog(qtbot: QtBot) -> None:
 
     project_dialog = ProjectDialog()
+    qtbot.addWidget(project_dialog)
 
 
-def test_project_dialog_main_window(qtbot) -> None:
+def test_project_dialog_main_window(
+        qtbot: QtBot,
+        setup_config: str
+) -> None:
 
-    core = FCore()
+    core = FCore(config_path=setup_config)
     main_window = MainWindow(core=core)
+    qtbot.addWidget(main_window)
 
     project_dialog = ProjectDialog(
         parent=main_window
     )
 
     assert project_dialog.main_window == main_window
+
 
 def test_project_tree_view() -> None:
 
