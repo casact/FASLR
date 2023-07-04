c8f5fa5b8454f8c29789bea467d66d52911a5c3a
Gene Dan <genedan@gmail.com>
2023-07-03 21:54:37 -0500

    FEAT: Add config_path as a parameter to FCore.
    This links the configuration file to the core.

--- faslr/core/core.py
+++ faslr/core/core.py
@@ -1,11 +1,19 @@
+import os
 from faslr.connection import get_startup_db_path
+from faslr.constants import CONFIG_PATH
 
 
 class FCore:
-    def __init__(self):
+    def __init__(
+            self,
+            config_path: str = CONFIG_PATH
+    ):
 
         # If a startup db has been indicated, get the path.
-        self.startup_db = get_startup_db_path()
+        if os.path.isfile(config_path):
+            self.startup_db = get_startup_db_path(config_path=config_path)
+        else:
+            self.startup_db = None
 
         # Flag to determine whether there is an active database connection. Most project-related functions
         # should be disabled unless a connection is established.
