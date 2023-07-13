62b992000b2076fafdd8373d9f41aecda6f5e0fe
Gene Dan <genedan@gmail.com>
2023-07-12 22:21:24 -0500

    TEST: Use tmp_path fixture to store test configuration file.

--- faslr/tests/conftest.py
+++ faslr/tests/conftest.py
@@ -4,20 +4,21 @@ import shutil
 
 from faslr.constants import (
     CONFIG_TEMPLATES_PATH,
-    DEFAULT_DIALOG_PATH,
-    ROOT_PATH
+    DEFAULT_DIALOG_PATH
 )
+
+from pathlib import Path
 
 
 @pytest.fixture()
-def setup_config() -> str:
+def setup_config(tmp_path: Path) -> str:
     """
     Use a temporary config file for the test.
     :return: str
     """
 
     if os.path.isfile(CONFIG_TEMPLATES_PATH):
-        dest_path = os.path.dirname(ROOT_PATH) + '/faslr_test.ini'
+        dest_path = tmp_path / 'faslr_test.ini'
         shutil.copy(CONFIG_TEMPLATES_PATH, dest_path)
     else:
         raise FileNotFoundError("CONFIG_PATH does not point to a valid config file.")
