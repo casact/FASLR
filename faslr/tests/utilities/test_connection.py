53165fbb454bc603fe6d30f471bd47f27a00f4b1
Gene Dan <genedan@gmail.com>
2023-07-12 22:22:21 -0500

    FIX: Correct path for test_connection.py.

--- faslr/tests/utilities/test_connection.py
+++ faslr/tests/utilities/test_connection.py
@@ -1,12 +1,15 @@
 from faslr.connection import FaslrConnection
+from faslr.constants import DEFAULT_DIALOG_PATH
 
 from faslr.utilities import set_sqlite_pragma
+
 
 def test_fk_pragma():
 
     fconn = FaslrConnection(
-        db_path='sample.db'
+        db_path=DEFAULT_DIALOG_PATH + '/sample.db'
     )
+
     set_sqlite_pragma(
         dbapi_connection=fconn.raw_connection,
         connection_record=None
