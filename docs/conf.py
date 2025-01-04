f3c19887212e7c15c8c215ae7527c0e8efe678a4
Gene Dan <genedan@gmail.com>
2025-01-04 13:56:39 -0600

    FIX: Move favicon code after pydata sphinx theme update.

--- docs/conf.py
+++ docs/conf.py
@@ -73,6 +73,7 @@ pygments_style = None
 # a list of builtin themes.
 #
 html_theme = 'pydata_sphinx_theme'
+html_favicon = '_static/favicon-32x32.png'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
