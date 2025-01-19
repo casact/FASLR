5e9ad4491e48c5efc053f28584103fdc81bc252d
Gene Dan <genedan@gmail.com>
2025-01-18 19:33:30 -0600

    FEAT: Deprecate index matrix standalone functions.

--- faslr/index/index_matrix.py
+++ faslr/index/index_matrix.py
@@ -32,42 +32,6 @@ if TYPE_CHECKING:
     from pandas import DataFrame
 
 
-# def relative_index(
-#         base_yr: int,
-#         years: list,
-#         index: list
-# ) -> dict:
-#     idx = years.index(base_yr)
-#     res = {}
-#     for x in range(len(years)):
-#         if years[x] < base_yr:
-#             adj = float(np.array(index[x+1:idx + 1]).prod()) ** - 1
-#         else:
-#             adj = np.array(index[idx + 1:x + 1]).prod()
-#         res[str(years[x])] = adj
-#     return res
-#
-#
-# def index_matrix(
-#     years: list,
-#     index: list
-# ) -> DataFrame:
-#
-#     d: list = []  # holds the data used to initialize the resulting dataframe
-#     for year in years:
-#         d += [relative_index(
-#             base_yr=year,
-#             years=years,
-#             index=index
-#         )]
-#
-#     df_res: DataFrame = pd.DataFrame(
-#         data=d,
-#         index=years
-#     )
-#
-#     return df_res
-
 class IndexMatrixModel(FAbstractTableModel):
     def __init__(
             self,
