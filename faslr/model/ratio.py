1dfc3ea0b9b55cdeb369ee6f417503f9d742b876
Gene Dan <genedan@gmail.com>
2025-01-27 20:16:50 -0600

    FEAT: Initialize base ratio selection class.

---
+++ faslr/model/ratio.py
@@ -0,0 +1,22 @@
+"""
+Module that contains base classes for ratio selections, e.g., development factor or a priori loss ratio selections.
+"""
+
+from PyQt6.QtWidgets import (
+    QWidget
+)
+
+
+class FRatioSelectionWidget(QWidget):
+    def __init__(self):
+        """
+        Containing widget for FRatioSelectionModel/View.
+        """
+        super().__init__()
+
+    def open_average_box(self):
+
+        pass
+
+
+
