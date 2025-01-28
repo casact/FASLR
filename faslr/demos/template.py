00fa8074978f7ccf3b26d0972ad944a981edbfae
Gene Dan <genedan@gmail.com>
2025-01-27 20:18:20 -0600

    FEAT: Update demo template to set sample db.

--- faslr/demos/template.py
+++ faslr/demos/template.py
@@ -3,9 +3,13 @@ Boilerplate code for writing demos.
 """
 import sys
 
+from faslr.demos.sample_db import set_sample_db
+
 from PyQt6.QtWidgets import (
     QApplication
 )
+
+set_sample_db()
 
 app = QApplication(sys.argv)
 
