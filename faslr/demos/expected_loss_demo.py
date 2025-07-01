76b7a9dc3c452f27f4c5d48d203897f156ea0eab
Gene Dan <genedan@gmail.com>
2025-06-30 21:40:28 -0500

    FEAT: Update expected_loss_demo.py to be consistent with XYZ Auto BI example.

--- faslr/demos/expected_loss_demo.py
+++ faslr/demos/expected_loss_demo.py
@@ -1,6 +1,8 @@
 import chainladder as cl
 import sys
 
+from faslr.demos.sample_db import set_sample_db
+
 from faslr.methods import (
     ExpectedLossWidget
 )
@@ -14,19 +16,20 @@ from PyQt6.QtWidgets import (
     QApplication
 )
 
+set_sample_db()
+
 app = QApplication(sys.argv)
 
-triangle = load_sample('auto_bi')
+triangle = load_sample('xyz')
 reported = triangle['Reported Claims']
 paid = triangle['Paid Claims']
 
-reported_dev = cl.TailConstant(tail=1.005).fit_transform(reported)
-reported_ult = cl.Chainladder().fit(reported_dev)
+reported_ult = cl.TailConstant(tail=1).fit_transform(cl.Development(n_periods=2, average='volume').fit_transform(reported))
+reported_ult = cl.Chainladder().fit(reported_ult)
 
-paid_dev = cl.TailConstant(tail=1.05).fit_transform(paid)
-paid_ult = cl.Chainladder().fit(paid_dev)
+paid_ult = cl.TailConstant(tail=1.010,decay=1).fit_transform(cl.Development(n_periods=2, average='volume').fit_transform(paid))
+paid_ult = cl.Chainladder().fit(paid_ult)
 
-test = table_from_tri(triangle=reported_ult)
 
 widget = ExpectedLossWidget(
     triangles=[reported_ult, paid_ult]
