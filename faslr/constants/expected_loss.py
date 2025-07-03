d3e5b16693ce443e70890f665c31a497155347cc
Gene Dan <genedan@gmail.com>
2025-07-03 17:47:09 -0500

    FEAT: Create default for expected loss raito averages.

---
+++ faslr/constants/expected_loss.py
@@ -0,0 +1,7 @@
+EXPECTED_LOSS_RATIO_AVERAGES = {
+            'Geometric': 'geometric',
+            'Medial': 'medial', # latest x years excluding high low
+            'Regression': 'regression',
+            'Straight': 'simple',
+            'Volume': 'volume'
+}
