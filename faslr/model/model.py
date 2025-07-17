07bfc04458cd285a92f374d13c9ad59634ebb2ca
Gene Dan <genedan@gmail.com>
2025-07-17 17:12:00 -0500

    FEAT: Annotate model.py.

--- faslr/model/model.py
+++ faslr/model/model.py
@@ -4,5 +4,10 @@ from PyQt6.QtWidgets import (
 
 
 class FModelWidget(QWidget):
+    """
+    Base model widget. Contains selection_tab, ibnr_summary.
+    """
     def __init__(self):
         super().__init__()
+
+        self.selection_tab = None
