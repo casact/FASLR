2455d47acfc071bf05c3b7be053627f56fa8b721
Gene Dan <genedan@gmail.com>
2026-01-13 07:55:29 -0600

    CHORE: Remove stub hierarchy query function (was moved to project.py).

--- faslr/utilities/queries.py
+++ faslr/utilities/queries.py
@@ -13,8 +13,3 @@ def delete_country(
     country = session.query(LocationTable).filter(LocationTable.location_id == country_id).one()
     session.delete(country)
     session.commit()
-
-def get_hierarchy() -> str:
-    """
-    Fetches the hierarchical position of the requested object in the project pane, i.e., country, state, LOB, etc.
-    """
