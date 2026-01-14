8dc7f532a3859bf0368760534fc7c30290629de0
Gene Dan <genedan@gmail.com>
2026-01-14 08:50:04 -0600

    TEST: Fix broken test.

--- faslr/tests/test_project_item.py
+++ faslr/tests/test_project_item.py
@@ -3,7 +3,8 @@ from faslr.project_item import ProjectItem
 def test_project_item() -> None:
 
     project_item = ProjectItem(
-        text="My Project"
+        text="My Project",
+        segment_level="country"
     )
 
     assert project_item.text() == "My Project"
